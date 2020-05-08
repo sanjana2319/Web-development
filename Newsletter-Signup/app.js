1ba95e12143f5db0687dba98cc8f03f816bf29ae
Your Name <2319sanjana@gmail.com>
2020-05-08 20:10:10 +0530

    go

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -16,9 +16,50 @@ app.post("/", function (req, res) {
     var lastName = req.body.lName;
     var email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subcribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                },
+            },
+        ],
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    var options = {
+        url: "http://us18.api.mailchimp.com/3.0/lists/9c5bc8e891",
+        method: "POST",
+        headers: {
+            Authorization: "sanjana 1eac024ee0b790f5a00541b983c32668-us18",
+        },
+        body: jsonData,
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            res.sendFile(__dirname + "/failure.html");
+        } else {
+            if (response.statusCode === 200) {
+                res.sendFile(__dirname + "/success.html");
+            } else {
+                res.sendFile(__dirname + "/failure.html");
+            }
+        }
+    });
+});
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
 });
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
+
+// 1eac024ee0b790f5a00541b983c32668-us18
+// 9c5bc8e891 List
