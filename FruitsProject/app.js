63bde9205f7d553c2e7c811e54a84464c669e4b7
Your Name <2319sanjana@gmail.com>
2020-07-13 10:56:55 +0530

    go

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,16 +1,94 @@
 const mongoose = require("mongoose");
+const { GridFSBucketWriteStream } = require("mongodb");
 
 mongoose.connect("mongodb://localhost:27017/fruitsDB", {
     useUnifiedTopology: true,
 });
 
-const fruitsSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+const fruitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is Required"],
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10,
+    },
     review: String,
 });
 
-const Fruit = mongoose.model("Fruit");
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+    name: "Apple",
+    rating: 8,
+    review: "Pretty solid",
+});
+// fruit.save();
+
+const peopleSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+});
+
+const Person = mongoose.model("Person", peopleSchema);
+const person = new Person({
+    name: "John",
+    age: 37,
+});
+
+// person.save();
+
+// const kiwi = new Fruit({
+//     name: "Kiwi",
+//     score: 10,
+//     review: "Awsome",
+// });
+
+// const orange = new Fruit({
+//     name: "Orange",
+//     score: 7,
+//     review: "okeish",
+// });
+
+// const banana = new Fruit({
+//     name: "Banana",
+//     score: 8,
+//     review: "Not Bad",
+// });
+
+// Fruit.insertMany([kiwi, orange, banana], function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else console.log("successfully added all the fruits");
+// });
+
+Fruit.find(function (err, fruits) {
+    if (err) {
+        console.log(err);
+    } else {
+        mongoose.connection.close();
+
+        fruits.forEach(function (fruit) {
+            console.log(fruit.name);
+        });
+    }
+});
+
+// Fruit.deleteOne({name : "Apples"}, function(err){
+//     if(err){
+//         console.log(err)
+//     }else
+//     console.log("Successfully eleted")
+// })
+Person.deleteMany({ name: "John" }, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Deleted Successfully!");
+    }
+});
 
 const insertDocuments = function (db, callback) {
     // Get the documents collection
